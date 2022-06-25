6f89e326b8834a6a686589c71afd5d2f39e12739
Sarahbrod <102585340+Sarahbrod@users.noreply.github.com>
2022-06-25 12:01:23 +1000

    Crowdfunding: Handling Model Relationships
    Added in pledges

--- crowdfunding/projects/models.py
+++ crowdfunding/projects/models.py
@@ -1,5 +1,18 @@
 from django.db import models
 
+
+class Pledge(models.Model):
+    amount = models.IntegerField()
+    comment = models.CharField(max_length=200)
+    anonymous = models.BooleanField()
+    project = models.ForeignKey(
+        'Project',
+        on_delete=models.CASCADE,
+        related_name='pledges'
+    )
+    supporter = models.CharField(max_length=200)
+
+
 class Project(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
