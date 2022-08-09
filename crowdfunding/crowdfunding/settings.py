f550fbd861415c9e9233d9306872d76dda2a697d
Sarahbrod <102585340+Sarahbrod@users.noreply.github.com>
2022-08-09 19:01:12 +1000

    fix: cors

--- crowdfunding/crowdfunding/settings.py
+++ crowdfunding/crowdfunding/settings.py
@@ -34,7 +34,7 @@ CORS_ALLOWED_ORIGINS = [
     "https://example.com",
     "https://sub.example.com",
     "http://localhost:8080",
-    "http://localhost:3000",
+    "http://localhost:3000/",
     "http://127.0.0.1:9000",
 ]
 
